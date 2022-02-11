e95930741d9453888fb16f4d9d14ac514f230de7
checkiamsiam <issiam02415@gmail.com>
2022-02-11 19:37:19 +0600

    js file composed

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -23,25 +23,42 @@ var withdrawField = document.getElementById('withdraw-field');
 var depositButton = document.getElementById('deposit-button');
 var withdrawButton = document.getElementById('withdraw-button');
 
+//common function
+function commonThing(field , amount , addIt ){
+  amount.innerText = Number(field.value ) + Number(amount.innerText);
+  if(addIt == true){
+    balanceAmount.innerText =Number(balanceAmount.innerText)  + Number(field.value) ;
+  }
+  if(addIt == false){
+    balanceAmount.innerText = Number(balanceAmount.innerText)-Number(field.value);
+  }
+  field.value = '';
+  if (balanceAmount.innerText == 'NaN' ) {
+    alert('You enterd a wrong value !!! please set a valid value to deposit money in your account');
+    location.reload();
+  }
+}
+
 //deposit section 
 depositButton.addEventListener('click', function () {
 // const current = depositAmount.innerText;
-  depositAmount.innerText = Number(depositField.value ) + Number(depositAmount.innerText);
+ /*  depositAmount.innerText = Number(depositField.value ) + Number(depositAmount.innerText);
   balanceAmount.innerText = Number(depositField.value) + Number(balanceAmount.innerText);
-  
   depositField.value = '';
-  if (balanceAmount.innerText == 'NaN' || depositAmount.innerText < 0) {
+  if (balanceAmount.innerText == 'NaN' ) {
     alert('You enterd a wrong value !!! please set a valid value to deposit money in your account');
     location.reload();
-  }
+  } */
+  commonThing(depositField , depositAmount , true);
 })
 
 withdrawButton.addEventListener('click', function () {
-  withdrawAmount.innerText = Number(withdrawField.value) + Number(withdrawAmount.innerText);
+  /* withdrawAmount.innerText = Number(withdrawField.value) + Number(withdrawAmount.innerText);
   balanceAmount.innerText = Number(balanceAmount.innerText) - Number(withdrawField.value);
   withdrawField.value = '';
-  if (balanceAmount.innerText == 'NaN' || balanceAmount.innerText < 0) {
+  if (balanceAmount.innerText == 'NaN' ) {
     alert('You enterd a wrong value !!! please set a valid value to deposit money in your account');
     location.reload();
-  }
+  } */
+  commonThing(withdrawField , withdrawAmount , false);
 })
